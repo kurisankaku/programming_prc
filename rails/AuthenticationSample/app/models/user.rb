b20e2fa922f18f061ff259dd5c90ce5e16d883a0
Kuriyama Munehisa <kurisankaku@gmail.com>
2016-11-16 18:40:42 +0900

    Modify user model to create user by oauth.

--- rails/AuthenticationSample/app/models/user.rb
+++ rails/AuthenticationSample/app/models/user.rb
@@ -5,4 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable,
          :omniauthable
+
+  def self.from_omniauth(auth)
+    user = where(provider: auth.provider, uid: auth.uid).first
+    if user.nil?
+      user = User.new
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+      user.token = auth.credentials.token
+
+      user.skip_confirmation!
+      user.save!
+    end
+    user
+  end
 end
